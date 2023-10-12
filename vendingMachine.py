5e447ac2f620e5ec2a863597c9b1390972f3f256
sarvinozsaidova <134921413+sarvinozsaidova@users.noreply.github.com>
2023-10-13 00:24:52 +0300

    second commit

--- vendingMachine.py
+++ vendingMachine.py
@@ -3,6 +3,7 @@ from beverage import Beverage
 class VendingMachine:
     def __init__(self):
         self.beverages = {}
+        self.cards = {}
 
     def addBeverage(self, beverage):
         self.beverages[beverage.getName()] = beverage
@@ -11,3 +12,15 @@ class VendingMachine:
         if beverageName not in self.beverages:
             return -1.0
         return self.beverages[beverageName].getPrice()
+    
+    def rechargeCard(self, card_id, credit):
+        if card_id in self.cards:
+            self.cards[card_id] += credit
+        else:
+            self.cards[card_id] = credit
+
+    def getCredit(self, card_id):
+        if card_id in self.cards:
+            return self.cards[card_id]
+        else:
+            return -1.0
